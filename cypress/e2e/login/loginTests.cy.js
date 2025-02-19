5b3e2e0d32e978cf7d512d3b6af926a1ebdf59a7
Emmie Brichet <brichetemmie@gmail.com>
2025-02-19 21:51:44 +0100

    without comment

--- cypress/e2e/login/loginTests.cy.js
+++ cypress/e2e/login/loginTests.cy.js
@@ -2,18 +2,16 @@ describe('Test de connexion', () => {
   it('Devrait permettre à l\'utilisateur de se connecter et de voir le lien vers le panier', () => {
     cy.visit('http://localhost:8080/#/login');
 
-    // Vérifier que le formulaire est affiché
+   
     cy.get('h1').should('contain', 'Se connecter');
     cy.get('form').should('be.visible');
 
-    // Saisir l'email et le mot de passe
+    
     cy.get('input#username').type('test2@test.fr');
     cy.get('input#password').type('testtest');
 
-    // Soumettre le formulaire
     cy.get('button[data-cy="login-submit"]').click();
 
-    // Vérifier que l'utilisateur est connecté et que le lien vers le panier est visible
     cy.url().should('not.include', '/login');
     cy.get('a[data-cy="nav-link-cart"]').should('be.visible');
   });
