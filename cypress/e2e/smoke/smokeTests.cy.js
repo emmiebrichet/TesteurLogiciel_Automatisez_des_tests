73606be180a6b910f48648b895aa4cd475354be8
Emmie Brichet <brichetemmie@gmail.com>
2025-03-20 17:26:54 +0100

    add a test on smoke test  on pavibility of product

--- cypress/e2e/smoke/smokeTests.cy.js
+++ cypress/e2e/smoke/smokeTests.cy.js
@@ -1,11 +1,11 @@
 describe('Smoke Test - Vérification des éléments sur la page d\'accueil', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:8080#/');
+    cy.visit('http://localhost:8080#/'); // Accès à la page d'accueil
   });
 
   it('Vérifier la présence des champs et boutons de connexion', () => {
-    cy.get('[data-cy="nav-link-login"]').should('exist'); 
+    cy.get('[data-cy="nav-link-login"]').should('exist'); // Vérification du bouton de connexion
   });
 
   it('Vérifier la présence des boutons d\'ajout au panier quand l\'utilisateur est connecté', () => {
@@ -14,23 +14,27 @@ describe('Smoke Test - Vérification des éléments sur la page d\'accueil', () 
     cy.get('input#password').type('testtest');
     cy.get('button[data-cy="login-submit"]').click();
 
-    cy.get('[data-cy="nav-link-logout"]').should('contain', 'Déconnexion');
-    cy.get('[data-cy="product-home-link"]').should('exist');
-    cy.get('[data-cy="nav-link-cart"]').should('exist');
+    cy.get('[data-cy="nav-link-logout"]').should('contain', 'Déconnexion'); // Vérification de la déconnexion
+    cy.get('[data-cy="nav-link-cart"]').should('exist'); // Vérification du lien vers le panier
+  });
 
-    it('Vérifier la présence et le contenu des informations des produits', () => {
-      cy.get('[data-cy="product-home"]').each(($product) => {
-        cy.wrap($product).find('[data-cy="product-home-img"]').should('exist');
-        cy.wrap($product).find('[data-cy="product-home-name"]').should('exist').and('not.be.empty');
-        cy.wrap($product).find('[data-cy="product-home-ingredients"]').should('exist').and('not.be.empty');
-        cy.wrap($product).find('[data-cy="product-home-price"]').should('exist').and('not.be.empty');
-        cy.wrap($product).find('[data-cy="product-home-link"]').should('exist');
-      });
+  it('Vérifier la présence et le contenu des informations des produits', () => {
+    cy.get('[data-cy="product-home"]').each(($product) => {
+      cy.wrap($product).find('[data-cy="product-home-img"]').should('exist');
+      cy.wrap($product).find('[data-cy="product-home-name"]').should('exist').and('not.be.empty');
+      cy.wrap($product).find('[data-cy="product-home-ingredients"]').should('exist').and('not.be.empty');
+      cy.wrap($product).find('[data-cy="product-home-price"]').should('exist').and('not.be.empty');
+      cy.wrap($product).find('[data-cy="product-home-link"]').should('exist');
     });
-    
+  });
+
+  it('Vérifier la présence du champ de disponibilité du produit', () => {
+    cy.get('[data-cy="product-home"]').each(($product) => {
+      cy.wrap($product).find('[data-cy="product-home-availability"]').should('exist').and('not.be.empty');
     });
+  });
+
 });
-
 
   
 
