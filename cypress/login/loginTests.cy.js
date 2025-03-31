b5eb2d3344de81ea13926f7835fc5af010e6d941
Emmie Brichet <brichetemmie@gmail.com>
2025-03-31 12:54:04 +0200

    edit login

--- cypress/login/loginTests.cy.js
+++ cypress/login/loginTests.cy.js
@@ -1,15 +1,10 @@
 describe('Test de connexion', () => {
   it('Devrait permettre à l\'utilisateur de se connecter et de voir le lien vers le panier', () => {
-    cy.visit('http://localhost:8080/#/login');
-
-   
+    cy.visit('http://localhost:8080/#/login');   
     cy.get('h1').should('contain', 'Se connecter');
     cy.get('form').should('be.visible');
-
-    
     cy.get('input#username').type('test2@test.fr');
     cy.get('input#password').type('testtest');
-
     cy.get('button[data-cy="login-submit"]').click();
     cy.url().should('not.include', '/login');
     cy.get('a[data-cy="nav-link-cart"]').should('be.visible');
@@ -20,11 +15,7 @@ describe('Test de connexion', () => {
     cy.get('[data-cy="login-input-username"]').type('wrong@test.fr');
     cy.get('[data-cy="login-input-password"]').type('wrongpass');
     cy.get('[data-cy="login-submit"]').click();
-    
-  
     cy.get('[data-cy="login-errors"]').should('contain', 'Identifiants incorrects');
-    
-   
     cy.get('a[data-cy="nav-link-cart"]').should('not.exist');
   });
 });
@@ -40,7 +31,6 @@ describe('Tests de sécurité XSS sur la page de connexion', () => {
     cy.get('[data-cy="login-input-username"]').type('<b>Test XSS</b>', { delay: 0 });
     cy.get('[data-cy="login-input-password"]').type('testtest');
     cy.get('[data-cy="login-submit"]').click();
-
     cy.get('[data-cy="login-errors"]').should('contain', 'Merci de remplir correctement tous les champs');
   });
 
@@ -48,11 +38,9 @@ describe('Tests de sécurité XSS sur la page de connexion', () => {
     cy.get('[data-cy="login-input-username"]').type('<script>alert("XSS")</script>', { delay: 0 });
     cy.get('[data-cy="login-input-password"]').type('testtest');
     cy.get('[data-cy="login-submit"]').click();
-
     cy.on('window:alert', (text) => {
       throw new Error(`Alerte détectée : ${text}`);
     });
-
     cy.get('[data-cy="login-errors"]').should('not.contain', '<script>');
     cy.get('[data-cy="login-errors"]').should('contain', 'Merci de remplir correctement tous les champs');
   });
@@ -61,11 +49,9 @@ describe('Tests de sécurité XSS sur la page de connexion', () => {
     cy.get('[data-cy="login-input-username"]').type('<img src=x onerror=alert("XSS")>', { delay: 0 });
     cy.get('[data-cy="login-input-password"]').type('testtest');
     cy.get('[data-cy="login-submit"]').click();
-
     cy.on('window:alert', (text) => {
       throw new Error(`Alerte détectée : ${text}`);
     });
-
     cy.get('[data-cy="login-errors"]').should('not.contain', '<img');
     cy.get('[data-cy="login-errors"]').should('contain', 'Merci de remplir correctement tous les champs');
   });
@@ -74,11 +60,9 @@ describe('Tests de sécurité XSS sur la page de connexion', () => {
     cy.get('[data-cy="login-input-username"]').type('<input onfocus=alert("XSS")>', { delay: 0 });
     cy.get('[data-cy="login-input-password"]').type('testtest');
     cy.get('[data-cy="login-submit"]').click();
-
     cy.on('window:alert', (text) => {
       throw new Error(`Alerte détectée : ${text}`);
     });
-
     cy.get('[data-cy="login-errors"]').should('not.contain', '<input onfocus');
     cy.get('[data-cy="login-errors"]').should('contain', 'Merci de remplir correctement tous les champs');
   });
@@ -87,31 +71,21 @@ describe('Tests de sécurité XSS sur la page de connexion', () => {
     cy.get('[data-cy="login-input-username"]').type('<a href="javascript:alert(\'XSS\')">Click me</a>', { delay: 0 });
     cy.get('[data-cy="login-input-password"]').type('testtest');
     cy.get('[data-cy="login-submit"]').click();
-
     cy.on('window:alert', (text) => {
       throw new Error(`Alerte détectée : ${text}`);
     });
-
     cy.get('[data-cy="login-errors"]').should('not.contain', '<a href="javascript');
     cy.get('[data-cy="login-errors"]').should('contain', 'Merci de remplir correctement tous les champs');
   });
 
   it('Ne doit pas exécuter du JavaScript lors de la connexion', () => {
     const xssPayload = '<script>alert("XSS")</script>';
-  
     cy.get('[data-cy="login-input-username"]').type(xssPayload);
     cy.get('[data-cy="login-input-password"]').type(xssPayload);
-  
-    
     cy.get('[data-cy="login-submit"]').click();
-  
-    
     cy.on('window:alert', (str) => {
       throw new Error(`XSS détecté lors de la connexion : ${str}`);
     });
-  
-    
-    
     cy.get('[data-cy="login-errors"]').should('contain', 'Merci de remplir correctement tous les champs');
   });
   
